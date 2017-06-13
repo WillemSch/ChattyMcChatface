231d09af21a6343252100d9bbb32b09907ffd25f
s1856316 <schooltink.willem@gmailcom>
2017-06-13 14:59:50 +0200

    push

--- app/src/main/java/UI/ChatButtonAdapter.java
+++ app/src/main/java/UI/ChatButtonAdapter.java
@@ -9,9 +9,6 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-import com.example.willem.chattymcchatface.R;
-
-import java.util.ArrayList;
 import java.util.List;
 
 /**
